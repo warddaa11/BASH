70ba6de8f44f2a282beee460b631f95f9a9fc934
warddaa11 <warda1169@hotmail.com>
2026-04-25 12:24:24 +0100

    Day2 bash

---
+++ Day2/exx.sh
@@ -0,0 +1,8 @@
+echo "enter a number:"
+read number
+if [ $number -gt 10 ]
+then
+	echo "big number"
+else
+	echo "small number"
+fi
---
+++ Day2/student_project/manager.sh
@@ -0,0 +1,9 @@
+Welcome student manager
+echo "What's you name."
+read x
+echo "How old are you?"
+read y
+echo "Where do you live?"
+read z
+echo "Student: $x, Age: $y, City: $z"
+echo "$x - $y - $z" >> students.txt
