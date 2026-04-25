5c23873a307a0a42d0299d4394e9a54e6fd0e10f
warddaa11 <warda1169@hotmail.com>
2026-04-25 12:23:29 +0100

    Day1 bash

---
+++ Day1/ex.sh
@@ -0,0 +1,5 @@
+echo "Enter your name:"
+read name
+echo "Enter your favorite language:"
+read language
+echo "Hello $name, you like $language"
---
+++ Day1/user.sh
@@ -0,0 +1,3 @@
+x="Ouarda"
+y=20
+echo "My name is $x and i am $y years old"
